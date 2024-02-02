d80a5875bd29072953747708f54268a25adb53f7
Alec <50636059+alec-zaiane@users.noreply.github.com>
2024-02-01 23:34:10 -0700

    add colour to MultiPointStar.java

--- app/src/main/java/MultiPointStar.java
+++ app/src/main/java/MultiPointStar.java
@@ -1,5 +1,6 @@
 public class MultiPointStar extends Shape{
     int numberOfPoints;
+    String colour = "Blue";
 
     public MultiPointStar(int numberOfPoints){
         this.numberOfPoints = numberOfPoints;
